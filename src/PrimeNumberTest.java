3321ca5c98c3e1c03f0074948f78d24db79def42
Carl <carln@foxmail.com>
2021-01-27 10:14:09 +0800

    Prime Number Test

--- src/PrimeNumberTest.java
+++ src/PrimeNumberTest.java
@@ -1,12 +1,15 @@
 public class PrimeNumberTest {
     public static void main(String[] args) {
 
+        long start = System.currentTimeMillis();
+
         boolean mark = true;    //标识符
 
-        for (int i = 2; i <= 1000; i++) {
-            for (int j = 2; j < i; j++) {
+        for (int i = 2; i <= 100000; i++) {
+            for (int j = 2; j < Math.sqrt(i); j++) {        //优化二，减少运算次数
                 if (i % j == 0) {
                     mark = false;
+                    break;      //优化一
                 }
             }
             if (mark == true) {
@@ -14,6 +17,11 @@ public class PrimeNumberTest {
             }
             mark = true;        //重置mark值
         }
+        long end = System.currentTimeMillis();
+        System.out.println("Time: " + (end - start));
+        //无break：13490
+        //有break：1145
+        //有sqrt(i)：83
     }
 }
 
