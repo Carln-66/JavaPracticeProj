c11000655c8e7eca3ecefc8cbe3a40d1693e6c4a
Carl <carln@foxmail.com>
2021-01-27 09:53:19 +0800

    Prime Number Test

---
+++ src/PrimeNumberTest.java
@@ -0,0 +1,19 @@
+public class PrimeNumberTest {
+    public static void main(String[] args) {
+
+        boolean mark = true;
+
+        for (int i = 2; i <= 1000; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    mark = false;
+                }
+            }
+            if (mark == true) {
+                System.out.println(i);
+            }
+            mark = true;
+        }
+    }
+}
+
